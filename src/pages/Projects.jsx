d9fa1a0bbcf7cc33e2eaf62ba800a1265bf7bd0a
napthedev <phongna.dev@gmail.com>
2022-05-28 23:31:32 +0700

    Feat: add pagination

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,10 @@
+import { joinQueryStrings, paginationRange } from "../shared/utils";
+
 import Footer from "../components/Footer";
 import { GET_ALL_PROJECTS } from "../graphql/queries";
 import { Link } from "react-router-dom";
 import Search from "../components/Search";
 import dayjs from "dayjs";
-import { joinQueryStrings } from "../shared/utils";
 import { useQuery } from "@apollo/client";
 import { useSearchQuery } from "../hooks/useSearchQuery";
 
@@ -118,7 +119,7 @@ export default function Projects() {
         </div>
       </div>
 
-      <div className="flex gap-3 justify-between flex-wrap items-center mx-[3vw] mt-6 min-h-[36px]">
+      <div className="flex gap-3 justify-center flex-wrap items-center mx-[3vw] mt-6 min-h-[36px]">
         {!loading && (
           <>
             <Link
@@ -129,16 +130,48 @@ export default function Projects() {
               })}`}
               className={`${
                 !data.projectsConnection.pageInfo.hasPreviousPage
-                  ? "cursor-default pointer-events-none !bg-primary-disabled"
+                  ? "cursor-default pointer-events-none !text-gray-500"
                   : ""
-              } bg-primary hover:brightness-125 transition duration-200 px-3 py-1 text-lg text-white`}
+              } w-8 h-8 text-lg text-white border rounded border-gray-300 flex justify-center items-center`}
             >
-              Trang trước
+              <svg
+                className="w-5 h-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 320 512"
+              >
+                <path
+                  className={
+                    !data.projectsConnection.pageInfo.hasPreviousPage
+                      ? "fill-gray-300"
+                      : "fill-gray-500"
+                  }
+                  d="M224 480c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l169.4 169.4c12.5 12.5 12.5 32.75 0 45.25C240.4 476.9 232.2 480 224 480z"
+                />
+              </svg>
             </Link>
-            <p className="text-xl">
-              Trang {page} trên{" "}
-              {Math.ceil(data.projectsConnection.aggregate.count / 8)}
-            </p>
+            {paginationRange({
+              total: Math.ceil(data.projectsConnection.aggregate.count / 8),
+              page: +page,
+            }).map((item) =>
+              item === "..." ? (
+                <span>...</span>
+              ) : (
+                <Link
+                  to={`/projects?${joinQueryStrings({
+                    page: +item,
+                    type: projectType,
+                    q: query,
+                  })}`}
+                  className={`${
+                    +page === item
+                      ? "bg-primary text-white"
+                      : "text-gray-700 border border-gray-300"
+                  } w-8 h-8 text-lg rounded flex justify-center items-center`}
+                >
+                  {item}
+                </Link>
+              )
+            )}
             <Link
               to={`/projects?${joinQueryStrings({
                 page: +page + 1,
@@ -147,12 +180,24 @@ export default function Projects() {
               })}`}
               className={`${
                 !data.projectsConnection.pageInfo.hasNextPage
-                  ? "cursor-default pointer-events-none !bg-primary-disabled"
+                  ? "cursor-default pointer-events-none !text-gray-500"
                   : ""
-              }
-            bg-primary hover:brightness-125 transition duration-200 px-3 py-1 text-lg text-white`}
+              } w-8 h-8 text-lg text-white border rounded border-gray-300 flex justify-center items-center`}
             >
-              Trang sau
+              <svg
+                className="w-5 h-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 320 512"
+              >
+                <path
+                  className={
+                    !data.projectsConnection.pageInfo.hasNextPage
+                      ? "fill-gray-300"
+                      : "fill-gray-500"
+                  }
+                  d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"
+                />
+              </svg>
             </Link>
           </>
         )}
--- src/shared/utils.js
+++ src/shared/utils.js
@@ -6,3 +6,51 @@ export const joinQueryStrings = (obj) =>
       return acc;
     }, [])
     .join("&");
+
+const range = (start, end) => {
+  const length = end - start + 1;
+  return Array.from({ length }, (_, index) => index + start);
+};
+
+export const paginationRange = ({
+  total,
+  page,
+  siblings = 1,
+  boundaries = 1,
+}) => {
+  const totalPageNumbers = siblings * 2 + 3 + boundaries * 2;
+
+  if (totalPageNumbers >= total) return range(1, total);
+
+  const leftSiblingIndex = Math.max(page - siblings, boundaries);
+  const rightSiblingIndex = Math.min(page + siblings, total - boundaries);
+
+  const shouldShowLeftDots = leftSiblingIndex > boundaries + 2;
+  const shouldShowRightDots = rightSiblingIndex < total - (boundaries + 1);
+
+  if (!shouldShowLeftDots && shouldShowRightDots) {
+    const leftItemCount = siblings * 2 + boundaries + 2;
+    return [
+      ...range(1, leftItemCount),
+      "...",
+      ...range(total - (boundaries - 1), total),
+    ];
+  }
+
+  if (shouldShowLeftDots && !shouldShowRightDots) {
+    const rightItemCount = boundaries + 1 + 2 * siblings;
+    return [
+      ...range(1, boundaries),
+      "...",
+      ...range(total - rightItemCount, total),
+    ];
+  }
+
+  return [
+    ...range(1, boundaries),
+    "...",
+    ...range(leftSiblingIndex, rightSiblingIndex),
+    "...",
+    ...range(total - boundaries + 1, total),
+  ];
+};
